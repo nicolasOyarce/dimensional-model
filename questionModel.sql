b231e671072f637dc1d3375b79ce5c0e75d26c4f
nicolasOyarce <niqo.oyarce24@gmail.com>
2024-07-17 21:55:30 -0400

    update query model

--- questionModel.sql
+++ questionModel.sql
@@ -12,7 +12,8 @@ WHERE DT.ANNIO = 2005
 	OR (DT.MES = 12 AND DT.DIA <= 24))
 GROUP BY ANNIO, MES, DIA
 ORDER BY MES;
-	
+GO
+
 -- 2. Cual fue la cantidad de unidades de peliculas arrendadas de la categoria "Family" durante el mes de julio de 2005?
 SELECT 
 	DC.CATEGORIA AS CATEGORIA,
@@ -26,6 +27,7 @@ WHERE DT.ANNIO = 2005
 	AND DT.MES = 7
 	AND DC.CATEGORIA = 'Family'
 GROUP BY DC.CATEGORIA, ANNIO, MES;
+GO
 
 -- 3. Cuantas unidades de peliculas arrendadas con idioma ingles y de la categoria "games" se realizaron en la Tienda 1, en el mes julio del 2005
 SELECT 
@@ -45,6 +47,7 @@ WHERE DT.ANNIO = 2005
 	AND DL.LENGUAJE = 'English'
 	AND FR.ID_TIENDA = 1
 GROUP BY FR.ID_TIENDA, DC.CATEGORIA, DL.LENGUAJE, ANNIO, MES;
+GO
 
 -- 4. Cantidad de unidades de peliculas arrendadas de la categoria "Horror" por tienda durante el mes de Agosto de 2005
 SELECT 
@@ -60,6 +63,7 @@ WHERE DT.ANNIO = 2005
 	AND DT.MES = 8
 	AND DC.CATEGORIA = 'Horror'
 GROUP BY FR.ID_TIENDA, CATEGORIA, ANNIO, MES;
+GO
 
 -- 5. Cantidad de unidades de peliculas de la categoria "Music" en la Tienda 2, entre junio y agosto del 2005
 SELECT 
@@ -76,7 +80,7 @@ WHERE DT.ANNIO = 2005
 	AND DC.CATEGORIA = 'Music'
 	AND FR.ID_TIENDA = 2
 GROUP BY FR.ID_TIENDA, CATEGORIA, MES, ANNIO;
-
+GO
 -- 6. Cantidad de unidades de peliculas arrendadas de la categoria "Sports" en Australia durante agosto de 2005
 SELECT 
 	DD.PAIS AS PAIS,
@@ -93,6 +97,7 @@ WHERE DT.ANNIO = 2005
 	AND DC.CATEGORIA = 'Sports'
 	AND DD.PAIS = 'Australia'
 GROUP BY PAIS, CATEGORIA, ANNIO, MES;
+GO
 
 -- 7. Cantidad de unidades de peliculas arrendadas entre 2005 y 2006 con idioma Ingles
 SELECT 
@@ -105,6 +110,7 @@ FROM FACT_RENTAL FR
 WHERE DT.ANNIO IN (2005, 2006)
 	AND DL.LENGUAJE = 'English'
 GROUP BY LENGUAJE, ANNIO;
+GO
 
 -- 8. Peliculas arrendadas en 2005 con costo de reemplazo entre 15 y 25 dolares
 SELECT 
@@ -117,6 +123,7 @@ FROM FACT_RENTAL FR
 WHERE DT.ANNIO = 2005
 	AND DP.remplazar_costo BETWEEN 15 AND 25
 GROUP BY DP.remplazar_costo, ANNIO;
+GO
 
 -- 9. Peliculas arrendadas en la tienda 1 en 2005 entre enero y septiembre
 SELECT 
@@ -130,6 +137,7 @@ WHERE DT.ANNIO = 2005
 	AND DT.MES BETWEEN 1 AND 9
 	AND FR.ID_TIENDA = 1
 GROUP BY FR.ID_TIENDA, ANNIO, MES;
+GO
 
 -- 10. Monto total de ingresos por alquileres en la tienda 1 durante mayo de 2005
 SELECT 
@@ -144,27 +152,29 @@ WHERE DT.ANNIO = 2005
 	AND DT.MES = 5
 	AND DTT.ID_TIENDA = 1
 GROUP BY DTT.ID_TIENDA, ANNIO, MES;
+GO
 
--- 11. Cantidad promedio de dias de alquiler para peliculas de "Action" de abril a junio de 2005
-SELECT 
-	DC.CATEGORIA AS CATEGORIA,
+-- 11. Total de ingresos por arriendos de peliculas en Canada durante el mes de Julio del 2005
+SELECT
+	DD.PAIS AS PAIS,
 	DT.ANNIO AS ANNIO,
 	DT.MES AS MES,
-	AVG(CAST(FR.UNIDADES AS FLOAT)) AS PROMEDIO_DIAS_ALQUILER
+	SUM(FR.DINERO) AS TOTAL_INGRESOS
 FROM FACT_RENTAL FR
 	JOIN DIM_TIEMPO DT ON FR.ID_TIEMPO = DT.ID_TIEMPO
-	JOIN DIM_CATEGORIA DC ON FR.ID_CATEGORIA = DC.ID_CATEGORIA	
+	JOIN DIM_DIRECCION DD ON FR.ID_DIRECCION = DD.ID_DIRECCION
 WHERE DT.ANNIO = 2005
-	AND DT.MES BETWEEN 4 AND 6
-	AND DC.CATEGORIA = 'Action'
-GROUP BY CATEGORIA, ANNIO, MES;
+	 AND DT.MES = 7
+	 AND DD.PAIS = 'Canada'
+GROUP BY PAIS, ANNIO, MES;
+GO
 
 -- 12. Categoria mas alquilada por clientes de idioma ingles en 2006
 SELECT TOP 1
 	DC.CATEGORIA, 
 	DT.ANNIO,
 	DL.LENGUAJE AS LENGUAJE,
-	COUNT(*) AS TOTAL_UNIDADES
+	COUNT(FR.UNIDADES) AS TOTAL_UNIDADES
 FROM FACT_RENTAL FR
 	JOIN DIM_TIEMPO DT ON FR.ID_TIEMPO = DT.ID_TIEMPO
 	JOIN DIM_CATEGORIA DC ON FR.ID_CATEGORIA = DC.ID_CATEGORIA
@@ -172,31 +182,35 @@ FROM FACT_RENTAL FR
 WHERE DT.ANNIO = 2006
 	AND DL.LENGUAJE = 'English'
 GROUP BY DC.CATEGORIA, DT.ANNIO, DL.LENGUAJE;
+GO
 
--- 13. Costo promedio de reemplazo de peliculas alquiladas por clientes con mas de 5 annios de registro en 2005
-SELECT 
+-- 13. Cantidad de unidades de peliculas arrendadas en la ciudad de Lethbridge el dia 22 de Agosto 2005
+SELECT
+	DD.CIUDAD AS CIUDAD,
 	DT.ANNIO AS ANNIO,
-	AVG(DP.remplazar_costo) AS PROMEDIO_COSTO_REMPLAZO
+	DT.MES AS MES,
+	DT.DIA AS DIA,
+	SUM(FR.UNIDADES) AS TOTAL_UNIDADES
 FROM FACT_RENTAL FR
 	JOIN DIM_TIEMPO DT ON FR.ID_TIEMPO = DT.ID_TIEMPO
-	JOIN DIM_CLIENTE DC ON FR.ID_CLIENTE = DC.ID_CLIENTE
-	JOIN DIM_PELICULA DP ON FR.ID_PELICULA = DP.ID_FILM
+	JOIN DIM_DIRECCION DD ON FR.ID_DIRECCION = DD.ID_DIRECCION
 WHERE DT.ANNIO = 2005
-	AND DATEDIFF(YEAR, DC.CREAR_DATOS, DT.ID_TIEMPO) > 5
-GROUP BY ANNIO;
+	 AND DT.MES = 8
+	 AND DT.DIA = 22
+	 AND DD.CIUDAD = 'Lethbridge'
+GROUP BY CIUDAD, ANNIO, MES, DIA;
+GO
 
--- 14. Top 5 clientes con mayores ingresos por alquiler entre 2005 y 2006
-SELECT TOP 5 
-	DC.ID_CLIENTE, 
-	DC.NOMBRE, 
-	DC.APELLIDO,
-	DT.ANNIO,
-	SUM(FR.DINERO) AS TOTAL_INGRESOS
+-- 14. Cantidad de unidades de peliculas arrendadas por mes durante el 2006
+SELECT 
+	DT.ANNIO AS ANNIO, 
+	DT.MES AS MES,
+	SUM(FR.UNIDADES) AS TOTAL_UNIDADES
 FROM FACT_RENTAL FR
 	JOIN DIM_TIEMPO DT ON FR.ID_TIEMPO = DT.ID_TIEMPO
-	JOIN DIM_CLIENTE DC ON FR.ID_CLIENTE = DC.ID_CLIENTE
-WHERE DT.ANNIO IN (2005, 2006)
-GROUP BY DC.ID_CLIENTE, DC.NOMBRE, DC.APELLIDO, DT.ANNIO;	
+WHERE DT.ANNIO = 2006
+GROUP BY ANNIO, MES;
+GO
 
 -- 15. Ingreso total por alquileres de peliculas de "Horror" en la tienda 2 durante 2005
 SELECT 
@@ -211,6 +225,7 @@ WHERE DC.CATEGORIA = 'Horror'
 	AND FR.ID_TIENDA = 2
 	AND DT.ANNIO = 2005
 GROUP BY FR.ID_TIENDA, CATEGORIA, ANNIO;
+GO
 
 -- 16. Ingreso total por alquileres de todas las peliculas en la tienda 2 durante 2005
 SELECT 
@@ -222,6 +237,7 @@ FROM FACT_RENTAL FR
 WHERE DT.ANNIO = 2005
 	AND FR.ID_TIENDA = 2
 GROUP BY ID_TIENDA, ANNIO;
+GO
 
 -- 17. Unidades de peliculas alquiladas a clientes con mas de 1 annio de registro durante 2005
 SELECT 
@@ -234,8 +250,9 @@ FROM FACT_RENTAL FR
 WHERE DATEDIFF(YEAR, DC.CREAR_DATOS, GETDATE()) > 1
 	AND DT.ANNIO = 2005
 GROUP BY DC.CREAR_DATOS, ANNIO;
+GO
 
--- 18. Unidades de peliculas de "Animacion" alquiladas por clientes de habla inlgesa en 2005
+-- 18. Unidades de peliculas de "Animacion" alquiladas por clientes de habla inglesa en 2005
 SELECT 
 	DC.CATEGORIA AS CATEGORIA,
 	DL.LENGUAJE AS LENGUAJE,
@@ -249,6 +266,7 @@ WHERE DT.ANNIO = 2005
 	AND DC.CATEGORIA = 'Animation'
 	AND DL.LENGUAJE = 'English'
 GROUP BY CATEGORIA, LENGUAJE, ANNIO;
+GO
 
 -- 19. Ingreso total por arriendos de peliculas de "Viajes" en la tienda 1 durante el 2005 y 2006
 SELECT 
@@ -263,6 +281,7 @@ WHERE DT.ANNIO = 2005
 	AND DC.CATEGORIA = 'Travel'
 	AND FR.ID_TIENDA = 1
 GROUP BY FR.ID_TIENDA, CATEGORIA, ANNIO;
+GO
 
 -- 20. Cantidad de unidades de peliculas arrendadas por dia durante mayo de 2005
 SELECT 
@@ -275,6 +294,7 @@ FROM FACT_RENTAL FR
 WHERE DT.ANNIO = 2005
 	AND DT.MES = 5
 GROUP BY ANNIO, MES, DIA;
+GO
 
 -- 21. Total de ingresos por arriendos de peliculas de "Action" en Lethbridge, Canada durante los ultimos 6 meses de 2005
 SELECT
@@ -283,7 +303,7 @@ SELECT
 	DC.CATEGORIA AS CATEGORIA,
 	DT.ANNIO AS ANNIO,
 	DT.MES AS MES,
-	SUM(FR.DINERO) AS TOTAL_INGRESOS
+	SUM(FR.DINERO) AS TOTAL_INGRESOSv
 FROM FACT_RENTAL FR
 	JOIN DIM_TIEMPO DT ON FR.ID_TIEMPO = DT.ID_TIEMPO
 	JOIN DIM_CATEGORIA DC ON FR.ID_CATEGORIA = DC.ID_CATEGORIA
